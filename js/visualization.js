3b086db4f50695daaa8fd1fb5c8651c610492a44
Wantong Lin <sherrylinwt@gmail.com>
2023-11-30 23:36:33 -0500

    add interaction for room filters

--- js/visualization.js
+++ js/visualization.js
@@ -147,6 +147,37 @@ d3.select("#incomeFilter").on("click", function() {
 });
 
 // Update the visualization based on the selected filter
+// this function is not working entirely and still needs revision on how to correlate 
+// the instances of furnitures 
 function updateVisualization(filterType) {
-  // this will be implemented when we are settled with how to use furnitures to represent different categories. 
+  // shadow all furnitures
+  d3.selectAll('#vis-svg-2 g').style('opacity', 0.2);
+
+  // Based on the filter type, highlight the corresponding furniture
+  switch (filterType) {
+      case 'poverty':
+          d3.selectAll('#vis-svg-2 g').filter(function() {
+              return this.innerHTML.includes('bedsheet'); 
+          }).style('opacity', 1);
+          break;
+      case 'education':
+          d3.selectAll('#vis-svg-2 g').filter(function() {
+              return this.innerHTML.includes('table'); 
+          }).style('opacity', 1);
+          break;
+      case 'ownership':
+          d3.selectAll('#vis-svg-2 g').filter(function() {
+              return this.innerHTML.includes('bathtub'); 
+          }).style('opacity', 1);
+          break;
+      case 'income':
+          d3.selectAll('#vis-svg-2 g').filter(function() {
+              return this.innerHTML.includes('couch'); 
+          }).style('opacity', 1);
+          break;
+      default:
+          d3.selectAll('#vis-svg-2 g').style('opacity', 1);
+          break;
+  }
 }
+
