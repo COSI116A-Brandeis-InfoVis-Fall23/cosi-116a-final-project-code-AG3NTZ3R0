6a3c26298288031aa3e1caf4068897ee36f9c0f8
AG3NTZ3R0 <Z3R0@duck.com>
2023-12-13 19:12:26 -0500

    Update Furniture class creation methods
    This commit includes a refactor of the Furniture class, revising the creation methods for furniture objects. All furniture creation methods now include a 'metric' parameter used to adjust dimensions of the furniture object. Furthermore, extra methods have been added for calculating normalized average and scaling dimensions, allowing more precise embedding of the created objects.

--- js/furniture.js
+++ js/furniture.js
@@ -7,29 +7,44 @@ export class Furniture {
     }
 
     build() {
-
         // Bed
-        this.createBed('f-resources',70, 30, "green", 180);
-        this.createBed('f-totval', 60, 350, "green", 180);
-        this.createBed('f-fedtax', 200, 350, "red", 180);
+        this.createBed('f-resources', 'SPM Resources (AVG)', 70, 30, "green", 180);
+        this.createBed('f-totval', 'SPM Totval (AVG)', 60, 350, "green", 180);
+        this.createBed('f-fedtax', 'SPM FedTax (AVG)', 200, 350, "red", 180);
         // Bathtub
-        this.createBathtub('f-snapsub', 320, 30, "orange");
-        this.createBathtub('f-schlunch', 230, 110, "orange");
+        this.createBathtub('f-snapsub', 'SPM SnapSub (AVG)', 300, 30, "orange");
+        this.createBathtub('f-schlunch', 'SPM SchLunch (AVG)', 230, 110, "orange");
         // Dining table
-        this.createDiningTable('f-fica', 520, 340, "red");
+        this.createDiningTable('f-fica', 'Spm Fica (AVG)', 520, 340, "red");
         // Couch
-        this.createCouch('f-medxpns', 350, 225, "red", 300);
-        this.createCouch('f-wicval', 390, 340, "orange", 175);
-        this.createCouch('f-sttax',390,445,"red");
+        this.createCouch('f-medxpns', 'SPM MedXpns (AVG)', 350, 225, "red", 300);
+        this.createCouch('f-wicval', 'SPM WICval (AVG)', 390, 340, "orange", 175);
+        this.createCouch('f-sttax','SPM StTax (AVG)', 390,445,"red");
         // Other furniture
-        this.createRect('f-caphousesub', 400, 275, "orange");
-        this.createRect('f-engval', 610, 175, "orange");
-        this.createRect('f-capwkccxpns', 620, 30, "red");
+        this.createRect('f-caphousesub', 'SPM CapHouseSub (AVG)', 400, 275, "orange");
+        this.createRect('f-engval', 'SPM EngVal (AVG)', 610, 175, "orange");
+        this.createRect('f-capwkccxpns', 'SPM CapWkCCXpns (AVG)', 620, 30, "red");
     }
 
-    createBed(id, x, y, color, rotation) {
-        let w = 50;
+    calculateNormalizedAvg(metric) {
+        return (this.statistics[metric]['avg'] - this.statistics[metric]['min']) /
+            (this.statistics[metric]['max'] - this.statistics[metric]['min']);
+    }
+
+    scaleDimensions(w, h, metric) {
+        let norm_avg = this.calculateNormalizedAvg(metric);
+        let scaled_w = w * norm_avg;
+        let scaled_h = h * norm_avg;
+
+        return [scaled_w, scaled_h];
+    }
+
+
+    createBed(id, metric, x, y, color, rotation) {
+        let w = 100;
         let h = 1.25 * w;
+
+        [w, h] = this.scaleDimensions(w, h, metric)
         let g = this.fg.append("g").attr("id", id).attr("fill", `${color}`).attr("transform", `rotate(${rotation}, ${x + w / 2}, ${y + h / 2})`)
         // bedsheet
         g.append("rect").attr("x", x).attr("y", y).attr("rx", 5).attr("ry", 5).attr("width", w).attr("height", h).style("opacity", 0.4)
@@ -39,17 +54,21 @@ export class Furniture {
         g.append("rect").attr("x", x).attr("y", y + 0.3 * w).attr("rx", 5).attr("ry", 5).attr("width", w).attr("height", h - 0.3 * w).style("opacity", 0.4)
     }
 
-    createBathtub(id, x, y, color) {
-        let w = 50;
+    createBathtub(id, metric, x, y, color) {
+        let w = 100;
         let h = 0.6 * w;
+
+        [w, h] = this.scaleDimensions(w, h, metric)
         let g = this.fg.append("g").attr("id", id).attr("fill", `${color}`)
         g.append("rect").attr("x", x).attr("y", y).attr("width", w).attr("height", h).style("opacity", 0.5)
         g.append("rect").attr("x", x + w / 12).attr("y", y + h / 8).attr("rx", 4).attr("ry", 4).attr("width", w - w / 6).attr("height", h - h / 4).style("opacity", 0.5)
     }
 
-    createDiningTable(id, x, y, color, rotation) {
+    createDiningTable(id, metric, x, y, color, rotation) {
         let w = 100;
         let h = 0.6 * w;
+
+        [w, h] = this.scaleDimensions(w, h, metric)
         let g = this.fg.append("g").attr("id", id).attr("fill", `${color}`).attr("transform", `rotate(${rotation}, ${x + w / 2}, ${y + h / 2})`)
         // table
         g.append("rect").attr("x", x).attr("y", y).attr("width", w).attr("height", h).style("opacity", 0.7)
@@ -61,11 +80,13 @@ export class Furniture {
         g.append("rect").attr("x", x + 2 * (w - 2*w_chair) / 3 + w_chair).attr("y", y + h).attr("width", w_chair).attr("height", h_chair).style("opacity", 0.6)
     }
 
-    createCouch(id, x, y, color, rotation) {
+    createCouch(id, metric, x, y, color, rotation) {
         let w = 30;
         let h = 43;
         if (w < 50) {h = 0.85 * w;}
         if (w > 100) {h = 0.43 * w;}
+
+        [w, h] = this.scaleDimensions(w, h, metric)
         let g = this.fg.append("g").attr("id", id).attr("fill", `${color}`).attr("transform", `rotate(${rotation}, ${x + w / 2}, ${y + h / 2})`)
         g.append("rect").attr("x", x).attr("y", y).attr("rx", 5).attr("ry", 5).attr("width", w).attr("height", h).style("opacity", 0.4)
         g.append("rect").attr("x", x).attr("y", y).attr("rx", 3).attr("ry", 3).attr("width", w).attr("height", 0.4 * h).style("opacity", 0.5)
@@ -73,9 +94,11 @@ export class Furniture {
         g.append("rect").attr("x", x + 0.7 * w).attr("y", y).attr("rx", 3).attr("ry", 3).attr("width", 0.3 * w).attr("height", h).style("opacity", 0.5)
     }
 
-    createRect(id, x, y, color, rotation = 0) {
+    createRect(id, metric, x, y, color, rotation = 0) {
         let w = 30;
         let h = 1.25 * w;
+
+        [w, h] = this.scaleDimensions(w, h, metric)
         let rotationPoint = `rotate(${rotation}, ${x + w / 2}, ${y + h / 2})`;
         this.fg
             .append('g')
